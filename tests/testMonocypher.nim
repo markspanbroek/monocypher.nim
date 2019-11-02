04f3defe707ef9241be75ec7f4c41162c06f842c
Mark Spanbroek <mark@spanbroek.net>
2019-11-02 15:49:41 +0100

    Remvoe magic numbers

--- tests/testMonocypher.nim
+++ tests/testMonocypher.nim
@@ -3,7 +3,7 @@ import sysrandom
 import monocypher
 
 test "key exchange":
-  let secret1, secret2: Key = getRandomBytes(32)
+  let secret1, secret2: Key = getRandomBytes(sizeof(Key))
   let public1 = crypto_key_exchange_public_key(secret1)
   let public2 = crypto_key_exchange_public_key(secret2)
   let shared1 = crypto_key_exchange(secret1, public2)
@@ -11,7 +11,7 @@ test "key exchange":
   check shared1 == shared2
 
 test "signing":
-  let secretKey: Key = getRandomBytes(32)
+  let secretKey: Key = getRandomBytes(sizeof(Key))
   let publicKey: Key = crypto_sign_public_key(secretKey)
   let message = "hello"
 
