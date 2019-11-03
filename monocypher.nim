1d6f9a5686ede5d243e93c39b0093fc452e39ca4
Mark Spanbroek <mark@spanbroek.net>
2019-11-03 12:11:32 +0100

    Rename import: cimports -> c

--- monocypher.nim
+++ monocypher.nim
@@ -1,4 +1,4 @@
-from monocypher/cImports import nil
+from monocypher/cImports as c import nil
 import monocypher/cHelpers
 
 type
@@ -8,38 +8,38 @@ type
   Signature* = array[64, byte]
 
 func crypto_key_exchange_public_key*(secretKey: Key): Key =
-  cimports.crypto_key_exchange_public_key(result, secretKey)
+  c.crypto_key_exchange_public_key(result, secretKey)
 
 func crypto_key_exchange*(yourSecretKey, theirPublicKey: Key): Key =
-  discard cimports.crypto_key_exchange(result, yourSecretKey, theirPublicKey)
+  discard c.crypto_key_exchange(result, yourSecretKey, theirPublicKey)
 
 func crypto_sign_public_key*(secretKey: Key): Key =
-  cimports.crypto_sign_public_key(result, secretKey)
+  c.crypto_sign_public_key(result, secretKey)
 
 func crypto_sign*(secretKey, publicKey: Key, message: Bytes): Signature =
   let (messagePtr, messageLen) = pointerAndLength(message)
-  cimports.crypto_sign(result, secretKey, publicKey, messagePtr, messageLen)
+  c.crypto_sign(result, secretKey, publicKey, messagePtr, messageLen)
 
 func crypto_check*(signature: Signature, publicKey: Key, message: Bytes): bool =
   let (messagePtr, messageLen) = pointerAndLength(message)
-  result = cimports.crypto_check(signature, publicKey, messagePtr, messageLen) == 0
+  result = c.crypto_check(signature, publicKey, messagePtr, messageLen) == 0
 
 func crypto_lock*(key: Key, nonce: Nonce, plaintext: Bytes): (Mac, seq[byte]) =
   let (plainPtr, plainLen) = pointerAndLength(plaintext)
   var mac: Mac
   var ciphertext = newSeq[byte](plainLen)
-  cimports.crypto_lock(mac, addr ciphertext[0], key, nonce, plainPtr, plainLen)
+  c.crypto_lock(mac, addr ciphertext[0], key, nonce, plainPtr, plainLen)
   result = (mac, ciphertext)
 
 func crypto_unlock*(key: Key, nonce: Nonce, mac: Mac, ciphertext: Bytes): seq[byte] =
   let (cipherPtr, cipherLen) = pointerAndLength(ciphertext)
   result = newSeq[byte](cipherLen)
-  let unlockResult = cimports.crypto_unlock(addr result[0], key, nonce, mac, cipherPtr, cipherLen)
+  let unlockResult = c.crypto_unlock(addr result[0], key, nonce, mac, cipherPtr, cipherLen)
   if not unlockResult == 0:
     raise newException(IOError, "message corrupted")
 
 func crypto_wipe*(secret: pointer, size: uint) =
-  cimports.crypto_wipe(secret, size)
+  c.crypto_wipe(secret, size)
 
 func crypto_wipe*(secret: openArray[any]) =
   let (secretPtr, secretLen) = pointerAndLength(secret)
